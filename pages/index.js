3e241f8fa77367229e7af8544aa5476694809b38
tan <minhtanbg6601@gmail.com>
2021-03-01 20:26:35 +0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from 'src/App'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>A11 galery</title>
+        <meta property="og:url" content="https://a11galery.tk" />
+        <meta property="og:type" content="website" />
+        <meta property="fb:app_id" content="your fb app id" />
+        <meta
+          property="og:title"
+          content="A11 Offical website"
+        />
+        <meta name="twitter:card" content="summary" />
+        <meta
+          property="og:description"
+          content="A11 Offical website"
+        />
+        <meta property="og:image" content={"/bg/leader/leader1.JPG"} />
+    </Head>
       <App/>
     </>
   )
