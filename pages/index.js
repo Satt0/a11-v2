ffd9fb56695a4127eb53a5a37afccd6ee00c4a32
tan <minhtanbg6601@gmail.com>
2021-03-02 08:27:31 +0700

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import App from 'src/App'
-import Head from 'next/head'
+import "bootstrap/dist/css/bootstrap.min.css";
+import App from "src/App";
+import Head from "next/head";
 
-export default function Home({items}) {
+export default function Home({ items }) {
   console.log(items);
   return (
     <>
-    <Head>
-      <title>A11 gallery K52 2016-2019</title>
+      <Head>
+        <title>A11 K52 Lạng Giang số 1 </title>
         <meta property="og:url" content="https://a11galery.tk" />
-        <meta name="description" content="A11 k52 -  Trung học phổ thông Lạng Giang số 1!" />
+        <meta
+          name="description"
+          content="A11 k52 -  Trung học phổ thông Lạng Giang số 1! Đây là website trưng bày ảnh kỉ yếu của tập thể lớp A11 k52 Trường Trung học phổ thông Lạng Giang số 1."
+        />
         <meta property="og:type" content="website" />
         <meta property="fb:app_id" content="your fb app id" />
-        <meta
-          property="og:title"
-          content="A11 Offical website"
-        />
+        <meta property="og:title" content="A11 Offical website" />
 
         <meta name="twitter:card" content="summary" />
         <meta
@@ -23,17 +23,20 @@ export default function Home({items}) {
           content="A11 k52 -  Trung học phổ thông Lạng Giang số 1!"
         />
         <meta property="og:image" content={"/bg/leader/leader1.JPG"} />
-    </Head>
-      <App/>
+      </Head>
+      <App />
     </>
-  )
+  );
 }
-export async function getStaticProps(){
-  const url=process.env.NODE_ENV==='production'?'https://desolate-escarpment-45092.herokuapp.com':'http://localhost:1337'
-  const data=await fetch(`${url}/carousels`).then(res=>res.json())
+export async function getStaticProps() {
+  const url =
+    process.env.NODE_ENV === "production"
+      ? "https://desolate-escarpment-45092.herokuapp.com"
+      : "http://localhost:1337";
+  const data = await fetch(`${url}/carousels`).then((res) => res.json());
   return {
-    props:{
-      items:data
-    }
-  }
+    props: {
+      items: data,
+    },
+  };
 }
