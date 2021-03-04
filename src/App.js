d3e81bdc89da2cb288b902bd922964a18a0a45e1
tan <minhtanbg6601@gmail.com>
2021-03-04 18:49:31 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 // import { Route, Switch } from "react-router-dom";
-import Intro from './Components/Intro/Intro'
-import Galery from './Components/Galery/Galery.js';
-import Video from './Components/Contact/Contact'
-import Ending1 from './Components/Ending1/Ending1'
+import Intro from "./Components/Intro/Intro";
+import Galery from "./Components/Galery/Galery.js";
+import Video from "./Components/Contact/Contact";
+import Ending1 from "./Components/Ending1/Ending1";
 export default function App() {
- 
   return (
     <div className="App">
       <Header />
-      
-       <Intro/>
-        <Galery/>
-     
-        <Video/>
-        <Ending1/>
-       
-      
+
+      <Intro />
+      <Galery />
+
+      <Video />
+      <Ending1 />
     </div>
   );
 }
