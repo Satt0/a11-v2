76531828ea1c03be4dc1837cd6a46b6664f3b0a8
tan <minhtanbg6601@gmail.com>
2021-04-11 16:00:48 +0700

    ok

--- src/Components/Intro/FlipCard.js
+++ src/Components/Intro/FlipCard.js
@@ -1,30 +1,39 @@
-import  { useState, useEffect } from 'react'
-import {useSelector} from 'react-redux'
-import styles from 'styles/FlipCard.module.scss'
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import styles from "styles/FlipCard.module.scss";
+import { getImgPath } from "lib/ulti";
 
+export default function App() {
+  const img = useSelector((state) => state.bg);
+  const [index, set] = useState(() => {
+    if (img.length) {
+      return Math.floor(Math.random() * img.length);
+    }
+    return 0;
+  });
 
-export default function App () {
-    const img=useSelector(state=>state.bg)
-  const [index, set] = useState(()=>{
-    if(img.length){
-      return Math.floor(Math.random()*img.length)
-    }
-    return 0
-  })
- 
-  useEffect(() =>{
-    const a= setTimeout(() => set(state => (state + 1) % img.length), 6000)
-    return ()=>{
-        clearTimeout(a)
-    }
-  }, [index])
-  return (<div
-  key={"randomize"+img[index].id}
-    className={styles.bg}
-    style={{ backgroundImage: `url("${img[index].bg[0].url}")` }}
-  >
-          <div style={{width:0,height:0,overflow:'hidden',backgroundImage: `url("${img[(index+1)%img.length].bg[0].url}")`}}></div>
-  </div>)
-    
-  
+  useEffect(() => {
+    const a = setTimeout(() => set((state) => (state + 1) % img.length), 6000);
+    return () => {
+      clearTimeout(a);
+    };
+  }, [index]);
+  return (
+    <div
+      key={"randomize" + img[index].id}
+      className={styles.bg}
+      style={{ backgroundImage: `url("${getImgPath(img[index].bg[0].url)}")` }}
+    >
+      <div
+        style={{
+          width: 0,
+          height: 0,
+          overflow: "hidden",
+          backgroundImage: `url("${getImgPath(
+            img[(index + 1) % img.length].bg[0].url
+          )}")`,
+        }}
+      ></div>
+    </div>
+  );
 }
