ec57ad3839653df50aeaf68b8bc919db56d4488b
Tan Hoang <tan@Tans-MacBook-Air.local>
2021-04-20 20:34:37 +0700

    test commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,67 +1,51 @@
 import React, { useState, useEffect } from "react";
 import ScrollIntoView from "react-scroll-into-view";
-import {useSelector,useDispatch} from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 export default function Header() {
-  const currentTheme=useSelector(state=>state.currentTheme)
-  const theme=useSelector(state=>state.theme)
-  const dispatch = useDispatch()
+  const currentTheme = useSelector((state) => state.currentTheme);
+  const theme = useSelector((state) => state.theme);
+  const dispatch = useDispatch();
   const [toggle, setToggle] = useState(false);
   const [sc, setSc] = useState(true);
-  // useEffect(() => {
-  //  let a= setInterval(()=>{
-  //   dispatch({type:'changeTheme',payload:currentTheme})
-  //  },20000)
-  //  return (()=>{clearInterval(a)})
-  // }, []);
-  useEffect(()=>{
-    let a  
-    if(!sc){
-        a=setTimeout(()=>{
-          setSc(true)
-        },4000)
-      }
-      return ()=>{
-        clearTimeout(a)
-      }
-  },[sc])
+
+  useEffect(() => {
+    let a;
+    if (!sc) {
+      a = setTimeout(() => {
+        setSc(true);
+      }, 4000);
+    }
+    return () => {
+      clearTimeout(a);
+    };
+  }, [sc]);
 
   return (
     <>
-     <button
-            className={!toggle ? "btn-toggle show" : "btn-toggle"}
-            onClick={() => {
-              setToggle((a) => !a);
-            }}
-          >
-            {toggle ? "hide" : "show"}
-          </button>
-      <header
-       
-        id="header"
-        className={toggle ? "" : "noBg"}
-        // style={{opacity:toggle?'1':'1',background:toggle?`linear-gradient(190deg,${theme[currentTheme]},.4),hsla(0,0%,100%,.8) 65%)`:`linear-gradient(190deg,${theme[currentTheme]},.1),hsla(0,0%,100%,0) 35%)`}}
+      <button
+        className={!toggle ? "btn-toggle show" : "btn-toggle"}
+        onClick={() => {
+          setToggle((a) => !a);
+        }}
       >
-        <div className="main-header" >
+        {toggle ? "hide" : "show"}
+      </button>
+      <header id="header" className={toggle ? "" : "noBg"}>
+        <div className="main-header">
           <ScrollIntoView selector="#intro">
-            <div id="brand-logo" className="ml-5 ">  
-            <img
-              style={{ cursor: "pointer" }}
-              
-              
-              src="/favicon.ico"
-              width="60px"
-              height="60px"
-            />
-            
+            <div id="brand-logo" className="ml-5 ">
+              <img
+                style={{ cursor: "pointer" }}
+                src="/favicon.ico"
+                width="60px"
+                height="60px"
+              />
             </div>
           </ScrollIntoView>
-
-         
         </div>
         <div className={"dropdown "}>
-         
           <div className="link-container">
-          <ScrollIntoView selector="#story">
+            <ScrollIntoView selector="#story">
               <li>Welcome</li>
             </ScrollIntoView>
             <ScrollIntoView selector="#galery">
@@ -70,36 +54,19 @@ export default function Header() {
             <ScrollIntoView selector="#video">
               <li>Video</li>
             </ScrollIntoView>
-             
           </div>
         </div>
         <div className="sound-cloud-container">
-          <button
-            title="click to open sound cloud"
-            onClick={() => {
-              setSc((a) => !a);
-            }}
-          >
-            <img
-              src="/sc.png"
-              alt="soundclound icon"
-              width="32px"
-              height="32px"
-            />
-          </button>
           <iframe
-            className={sc ? "sc show" : "sc hide"}
-            width="300px"
-            height="400px"
+            width="100%"
+            height="380"
             scrolling="no"
-            frameBorder="no"
+            frameborder="no"
             allow="autoplay"
-            title="Sound clound"
-            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/929815216&color=%23444444&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/929815216&color=%23ff5500&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
           ></iframe>
         </div>
       </header>
     </>
   );
 }
-
