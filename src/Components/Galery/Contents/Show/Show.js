c6f41e45b3bddc103ec6b7e23cba832cb162da6e
Tan Hoang <tan@Tans-MacBook-Air.local>
2021-05-06 10:29:05 +0700

    update randomizing images

--- src/Components/Galery/Contents/Show/Show.js
+++ src/Components/Galery/Contents/Show/Show.js
@@ -3,7 +3,19 @@ import React from "react";
 import Viewpager from "./Viewpager/Viewpager";
 import { useSelector } from "react-redux";
 export default function Show({ length, view }) {
-  const img = useSelector((state) => state.img.filter((e) => e.view === view));
+  const img = useSelector((state) => {
+    const data=state.img.filter((e) => e.view === view)
+   if(data.length){
+    for(let i=data.length-1;i>=0;i--){
+      const index=Math.floor(Math.random()*i)
+      const temp=data[i]
+      data[i]=data[index]
+      data[index]=temp;
+  }
+   }
+   console.log('testing img');
+    return data
+  });
 
   return (
     <div className="Show">
