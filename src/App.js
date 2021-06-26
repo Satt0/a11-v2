9879e0c5af4982511ca1ed291525152af6f40546
tan <minhtanbg6601@gmail.com>
2021-06-26 10:25:33 +0700

    fix dynamic import

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import dynamic from 'next/dynamic'
-import {useState} from 'react'
+import React from 'react'
 import Display from "./Components/Display/Display";
 const Ending1=dynamic(()=>import('./Components/Ending1/Ending1'))
 import Intro from './Components/Intro/Intro'
- const DisplayImage=dynamic(()=>import ('./Components/ShowMember'))
+import DisplayImage from './Components/ShowMember/index'
+//  const DisplayImage=dynamic(()=>import ('./Components/ShowMember'))
  const Wrapper=dynamic(()=>import('./Components/Moment/Wrapper'))
 export default function App() {
-  const [isLoad,setIsload]=useState(false)
-
-const unLock=()=>{
-  console.log('unlocking',isLoad);
-  setIsload(true)
-}
+  const [isLock,setLock]=React.useState(true)
   return (
     <div
       className="App"
@@ -19,23 +15,23 @@ const unLock=()=>{
     >
       
       
-     <Intro unLock={unLock} />
+    {isLock&& <Intro unLock={()=>{setLock(false)}} />}
 
         
-         <>
+        {!isLock &&  <>
           
-            <Display />
-          <h1  className="title-header pt-5 text-center pb-5">#MEMBER</h1>
+          <Display />
+        <h1  className="title-header pt-5 text-center pb-5">#MEMBER</h1>
 
-      <DisplayImage/>
-          {/* <Galery /> */}
-          <h1  className="title-header pt-5 text-center pb-5">#MOMENT</h1>
+    <DisplayImage/>
+        {/* <Galery /> */}
+        <h1  className="title-header pt-5 text-center pb-5">#MOMENT</h1>
 
-      <Wrapper/>
-      {/* <Video/> */}
-        <Ending1 />
+    <Wrapper/>
+    {/* <Video/> */}
+      <Ending1 />
 
-          </>
+        </>}
       
     </div>
   );
